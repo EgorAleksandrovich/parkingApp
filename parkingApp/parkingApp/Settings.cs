1e80ff4c5d39298566e78b8a8254ac25071ef9ef
EgorAleksandrovich <bolshakov.egorr@gmail.com>
2018-05-09 22:41:06 +0300

    added dictionary price

--- parkingApp/parkingApp/Settings.cs
+++ parkingApp/parkingApp/Settings.cs
@@ -10,7 +10,15 @@ namespace parkingApp
     {
         private static int _parkingSpace = 100;
         private static int _fine = 2;
+        private static Dictionary<string,int> _parkingPrice = new Dictionary<string,int>
+        {
+            {"Truck", 5},
+            {"Passenger", 3},
+            {"Bus", 2},
+            {"Motorcycle", 1}
+        };
 
+        public static Dictionary<string, int> ParkingPrice { get { return _parkingPrice; } }
         public static int ParkingSpace { get { return _parkingSpace; } }
         public static int Fine { get { return _fine; } }
     }
