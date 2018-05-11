37c6f13ce9252e4cd7c950d98b6144942d474def
EgorAleksandrovich <bolshakov.egorr@gmail.com>
2018-05-11 11:19:44 +0300

    Created interface "IMenu"

---
+++ parkingApp/parkingApp/Interfaces/IMenu.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parkingApp
+{
+    public interface IMenu
+    {
+        void StartMenu();
+        void ParkingInfoMenu();
+        void ParkingPickUpTheCarMenu();
+    }
+}
