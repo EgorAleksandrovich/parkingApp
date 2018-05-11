11ff5d04055804d8059c5288ea893ef0745b23e1
EgorAleksandrovich <bolshakov.egorr@gmail.com>
2018-05-11 23:32:47 +0300

    Updated

--- parkingApp/parkingApp/Models/Car.cs
+++ parkingApp/parkingApp/Models/Car.cs
@@ -11,30 +11,5 @@ namespace parkingApp
         public string Id { get; set; }
         public CarType CarType { get; set; }
         public int Balance { get; set; }
-
-
-        public bool ReplenishAccount()
-        {
-            bool canseledAction = false;
-            string inputString = null;
-            int amount = 0;
-
-            while (amount <= 0)
-            {
-                Console.Write("Enter amount to replenish (enter \"x\" to cancel): ");
-                inputString = Console.ReadLine();
-                if(inputString.ToLower() == "x")
-                {
-                    canseledAction = true;
-                    break;
-                }
-                if (!int.TryParse(inputString, out amount) || amount <= 0)
-                {
-                    Console.WriteLine("Error! Invalid input. The text you enter must be an integer value greater than 0");
-                }
-                this.Balance += amount;
-            }
-            return canseledAction;
-        }
     }
 }
