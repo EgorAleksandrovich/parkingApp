b33d4b5df40c0ebaeea9d98f14aa4821ce366d3a
EgorAleksandrovich <bolshakov.egorr@gmail.com>
2018-05-11 22:09:20 +0300

    Remove all stupid logic
    Have been mistaken.

--- parkingApp/parkingApp/Program.cs
+++ parkingApp/parkingApp/Program.cs
@@ -10,60 +10,13 @@ namespace parkingApp
 {
     class Program
     {
-        static Parking _parking;
         static void Main(string[] args)
         {
+            Parking _parking;
             _parking = Parking.GetInstance();
             Menu menu = new Menu();
-            string previousUserChoice = null;
-            string currentUsersChoice = "StartMenu";
-            string newUserChoice = "";
-            
-            while (currentUsersChoice != "Exit")
-            {
-                newUserChoice = Program.CallMethod(currentUsersChoice, menu);
-                if (newUserChoice == currentUsersChoice)
-                {
-                    currentUsersChoice = previousUserChoice;
-                }
-                else
-                {
-                    if (previousUserChoice == null)
-                    {
-                        previousUserChoice = newUserChoice;
-                    }
-                    currentUsersChoice = newUserChoice;
-                }
-            }
+            menu.StartMenu(_parking);
             Console.ReadKey();
-        }
-        static string CallMethod(string methodName, Menu menu)
-        {
-            string result = methodName;
-            Type thisType = menu.GetType();
-            if (thisType != null)
-            {
-                MethodInfo methodInfo = thisType.GetMethod(methodName);
-
-                if (methodInfo != null)
-                {
-                    ParameterInfo[] parameters = methodInfo.GetParameters();
-                    if (parameters.Length == 0)
-                    {
-                        result = methodInfo.Invoke(menu, null).ToString();
-                    }
-                    else
-                    {
-                        object[] parametersArray = new object[] { _parking };
-                        object obj = methodInfo.Invoke(menu, parametersArray);
-                        if (obj != null)
-                        {
-                            result = obj.ToString();
-                        }
-                    }
-                }
-            }
-            return result;
         }
     }
 }
